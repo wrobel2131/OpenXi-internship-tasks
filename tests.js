a7aab2c47a571ee768d2c99929ddf3840f252c1e
Dawid <77682406+wrobel2131@users.noreply.github.com>
2022-04-26 02:47:42 +0200

    Delete tests.js

--- tests.js
+++
@@ -1,7 +0,0 @@
-import { manageAPIDataProgram } from "./program.js";
-import fetch from 'node-fetch';
-
-const usersApiUrl = "https://fakestoreapi.com/users";
-const cartsApiUrl = "https://fakestoreapi.com/carts";
-const productsApiUrl = "https://fakestoreapi.com/products";
-manageAPIDataProgram();
